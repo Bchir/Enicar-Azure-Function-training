4df6969a805cb0fc321d81ea4b390f20912d9ec4
Mohamed Amine bchir <bchir.mohamed.amine@outlook.com>
2025-02-22 10:19:45 +0100

    first commit

---
+++ src/functions/Calculator.js
@@ -0,0 +1,14 @@
+const { app } = require('@azure/functions');
+
+app.http('Calculator', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}"`);
+
+        const firstNumber = Number.parseInt(request.query.get('firstNumber'));
+        const secondNumber = Number.parseInt(request.query.get('SecondNumber'));
+
+        return { Sum: firstNumber + secondNumber };
+    }
+});
